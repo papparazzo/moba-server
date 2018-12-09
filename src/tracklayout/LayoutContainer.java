14ceab1517a275f4e3afd6520d75b2b680f06e07
papparazzo <pappi-@gmx.de>
2018-12-09 22:37:28 +0100

    container refactored

--- src/tracklayout/LayoutContainer.java
+++ src/tracklayout/LayoutContainer.java
@@ -21,41 +21,99 @@
 package tracklayout;
 
 import datatypes.base.Position;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Tracklayout {
+public class LayoutContainer {
+    protected final Map<Position, Symbol> map;
 
-    protected Symbol[][] layout;
+    protected int maxX = 0;
+    protected int maxY = 0;
 
-    protected int height;
-    protected int width;
-
-    public Tracklayout(int height, int width) {
-        this.height = height;
-        this.width = width;
-
-        layout = new Symbol[width][height];
+    public LayoutContainer() {
+        map = new HashMap<>();
     }
 
-    public void addSymbol(int x, int y, Symbol symbol) {
-        if(x >= width || y >= height) {
+    public Symbol put(int x, int y, Symbol value) {
+        return put(new Position(x, y), value);
+    }
+
+    public Symbol put(Position pos, Symbol value) {
+        if(maxX < pos.getX()) {
+            maxX = pos.getX();
+        }
+        if(maxY < pos.getY()) {
+            maxY = pos.getY();
+        }
+        return map.put(pos, value);
+    }
+
+    public Symbol get(Position pos) {
+        if(pos.getX() > maxX || pos.getY() > maxY) {
             throw new ArrayIndexOutOfBoundsException();
         }
-        layout[x][y] = new Symbol();
+
+        if(!map.containsKey(pos)) {
+            return null;
+        }
+        return map.get(pos);
     }
 
-    public Position getStartPoint() {
-        for(int y = 0; y < height; ++y) {
-            for(int x = 0; x < width; ++x) {
-                if(layout[x][y] != null) {
-                    return new Position(x, y);
+    public Symbol get(int x, int y) {
+        return get(new Position(x, y));
+    }
+
+    public void clear() {
+        map.clear();
+    }
+
+    public boolean containsKey(int x, int y) {
+        return containsKey(new Position(x, y));
+    }
+
+    public boolean containsKey(Position pos) {
+        return map.containsKey(pos);
+    }
+
+    public Position getNextFreePosition() {
+        return getNextFreePosition(new Position());
+    }
+
+    public Position getNextFreePosition(Position pos) {
+        for(int y = pos.getY(); y <= maxY; ++y) {
+            for(int x = pos.getX(); x <= maxX; ++x) {
+                Position tmp = new Position(x, y);
+                if(!map.containsKey(tmp)) {
+                    continue;
                 }
+                Symbol symbol = map.get(pos);
+                if(symbol.isSymbol()) {
+                    continue;
+                }
+                return tmp;
             }
         }
-        throw new ArrayIndexOutOfBoundsException("No start found");
+        throw new ArrayIndexOutOfBoundsException("No position found!");
     }
 
-    public boolean validate() {
-        return false;
+    public Position getNextBoundPosition() {
+        return getNextFreePosition(new Position());
     }
 
+    public Position getNextBoundPosition(Position pos) {
+        for(int y = pos.getY(); y <= maxY; ++y) {
+            for(int x = pos.getX(); x <= maxX; ++x) {
+                Position tmp = new Position(x, y);
+                if(!map.containsKey(tmp)) {
+                    continue;
+                }
+                Symbol symbol = map.get(pos);
+                if(!symbol.isSymbol()) {
+                    continue;
+                }
+                return tmp;
+            }
+        }
+        throw new ArrayIndexOutOfBoundsException("No position found!");
+    }
 }
