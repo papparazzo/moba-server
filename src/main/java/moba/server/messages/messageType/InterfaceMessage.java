08ad2f7f62f414ae70b4053fe5c5aaa8b1a434d6
papparazzo <pappi-@gmx.de>
2023-04-27 21:26:14 +0200

    message enhanced

--- src/main/java/moba/server/messages/messageType/InterfaceMessage.java
+++ src/main/java/moba/server/messages/messageType/InterfaceMessage.java
@@ -27,8 +27,11 @@ public enum InterfaceMessage implements MessageType {
     CONNECTIVITY_STATE_CHANGED(1),
     CONTACT_TRIGGERED         (2),
     SET_BRAKE_VECTOR          (3),
-    SET_LOCO_SPEED            (4),
-    SET_LOCO_DIRECTION        (5);
+    RESET_BRAKE_VECTOR        (4),
+    SET_LOCO_SPEED            (5),
+    SET_LOCO_DIRECTION        (6),
+    SET_LOCO_FUNCTION         (7),
+    SWITCH_ACCESSORY_DECODERS (8);
 
     public final static int GROUP_ID = 6;
 
