51720773c360df35996b48efd92b53758b118255
papparazzo <pappi-@gmx.de>
2025-03-22 13:32:39 +0100

    build new version

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # moba-server
+
+For further information please have a look at https://github.com/papparazzo/moba-docu
--- src/main/java/moba/server/App.java
+++ src/main/java/moba/server/App.java
@@ -30,7 +30,7 @@ import moba.server.application.ServerApplication;
 public class App {
     protected static final String INI_FILE_NAME = "config.yaml";
     protected static final String APP_NAME      = "appserver";
-    protected static final String APP_DATE      = "13.05.2011";
+    protected static final String APP_DATE      = "22.03.2025";
     protected static final String APP_VERSION   = "1.0.0-0000";
     protected static final String APP_AUTHOR    = "Stefan Paproth (Pappi-@gmx.de)";
 
